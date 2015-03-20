f88361bc05a1e28815ff0a0650f2eb8f407226a5
GiraudSophie <sophie.giraud@gmail.com>
2015-03-20 18:34:19 +0100

    creation de la page en html

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+	<head>
+		<title>Découverte Git</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	</head>
+	<body>
+		<h1>Git</h1>
+		
+		
+		<h1>GitHub</h1>
+	</body>
+</html>
